83c9501c7b6f3d1778678c4fb9895f5059e7916f
akash <akashgupta.webdev@gmail.com>
2026-06-25 14:15:51 +0530

    kkkkk

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,34 +1,23 @@
 import pino from "pino";
 import { env } from "./env.js";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export const logger = pino({
   name: env.APP_NAME,
   level: env.LOG_LEVEL,
-  transport: env.NODE_ENV === "development" ? {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "HH:MM:ss Z",
-      ignore: "pid,hostname",
-    },
-  } : undefined,
+  transport: isDevelopment
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
   base: {
-    environment: env.NODE_ENV,
+    environment: process.env.NODE_ENV,
     service: "ornacore-backend",
-  },
-  redact: {
-    paths: [
-      "req.headers.authorization",
-      "req.headers.cookie",
-      "password",
-      "passwordHash",
-      "token",
-      "accessToken",
-      "refreshToken",
-      "otp",
-      "*.password",
-      "*.token",
-    ],
-    censor: "[REDACTED]",
   },
 });
